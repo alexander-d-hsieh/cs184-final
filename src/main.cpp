924a84d7bb867ba50db7c76982c654517c628a63
Derek Kennedy <derekmk@berkeley.edu>
2018-04-30 21:28:17 -0700

    Constraint value correction

--- src/main.cpp
+++ src/main.cpp
@@ -348,10 +348,9 @@ void loadObjectsFromFile(string filename, BrittleObject *brittleObject, BrittleO
           double tri_area = (cross(triangle->v1->position - triangle->v2->position,
                                    triangle->v3->position - triangle->v2->position)).norm() / 2.0;
           double tet_volume = a->volume + b->volume;
-          int random = 20 * pn.noise(a->position.x, a->position.y, a->position.z);
+          double random = 20. * pn.noise(a->position.x, a->position.y, a->position.z);
           random -= floor(random);
-          double constraint_value = 
-              (tri_area + 0.5 * tet_volume) * random + op->constraint_strength_additive;
+          double constraint_value = (tri_area + 0.5 * tet_volume) * random;
           min_con = min(min_con, constraint_value);
           max_con = max(max_con, constraint_value);
           Constraint *c = new Constraint(a, b, constraint_value, false);
